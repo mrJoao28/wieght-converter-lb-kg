e16b855241e58fb536de9ea66f18ca06c89e9991
mrjhon <joaofelipeazevedo28@gmail.com>
2026-02-22 08:22:51 -0400

    new file:   main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qual é a medida (kg , lb): ");
+        String medida = scanner.nextLine();
+        medida = medida.trim().toLowerCase();
+        System.out.print("\n");
+        System.out.print("Qual é seu peso: ");
+        double weigth = scanner.nextDouble();
+        if (medida.equals("kg")){
+            System.out.printf("%.2f lb \n",weigth*2.20462);
+        }
+        else if (medida.equals("lb")){
+            System.out.printf("%.2f kg \n",weigth*0.453582);
+        }
+        else {
+            System.out.println("medida invalida");
+        }
+
+        scanner.close();
+
+    }
+
+}
